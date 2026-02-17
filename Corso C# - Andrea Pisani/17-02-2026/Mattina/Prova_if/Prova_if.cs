87bf9e3d8e96360afdb54c7090263f01b8660d52
andrea-pisani <andreap2007@proton.me>
2026-02-17 11:45:17 +0100

    Add files via upload

---
+++ Corso C# - Andrea Pisani/17-02-2026/Mattina/Prova_if/Prova_if.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program()
+{
+    static void Main(string[] args)
+    {
+        int a = 15, b = 10;
+        /* if (condizione == true)
+        {
+            esegui codice all'interno delle graffe
+        } */
+        if (a > b)
+        {
+            Console.WriteLine($"{a} è maggiore di {b}");
+        }
+    }
+}
