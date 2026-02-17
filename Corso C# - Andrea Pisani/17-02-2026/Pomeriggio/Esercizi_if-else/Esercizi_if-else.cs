0684f0d430e9601b66193f64068d8670b7239590
andrea-pisani <andreap2007@proton.me>
2026-02-17 15:28:54 +0100

    Add files via upload

---
+++ Corso C# - Andrea Pisani/17-02-2026/Pomeriggio/Esercizi_if-else/Esercizi_if-else.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program()
+{
+    static void Main(string[] args)
+    {
+        //----- ESERCIZIO 1
+        Console.WriteLine("Dammi un numero: ");
+        int n1 = int.Parse(Console.ReadLine());
+        
+        if(n1 % 2 == 0)
+        {
+            Console.WriteLine("Il numero è pari");
+        }
+        else
+        {
+            Console.WriteLine("Il numero è dispari");
+        }
+
+
+
+        //-----ESERCIZIO 2
+        const int PASSWORD = 12345678;
+        Console.WriteLine("Inserire password: ");
+        int pass = int.Parse(Console.ReadLine());
+
+        if(pass == PASSWORD)
+        {
+            Console.WriteLine("Accesso consentito");
+        }
+        else
+        {
+            Console.WriteLine("Accesso negato");
+        }
+
+
+
+        //-----ESERCIZIO 3
+        Console.WriteLine("Inserire un numero: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Inserire un'altro numero: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Inserire un'operatore (+ oppure -): ");
+        char op = char.Parse(Console.ReadLine());
+        if(op != '+' && op != '-')
+        {
+            Console.WriteLine("Operatore non valido!");
+        }
+        if(op == '+')
+        {
+            Console.WriteLine($"La somma è: {a + b}");
+        }
+        if(op == '-')
+        {
+            Console.WriteLine($"La differenza è: {a - b}");
+        }
+    }
+}
